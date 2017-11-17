3870a0d861b828dd9e0e013df8ceaeb6921bb353
Atnnas <david.artavia.rodriguez@gmail.com>
2017-11-17 16:17:29 -0600

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,10 +1,20 @@
 $( document ).ready(function() {
-	$('.navbar').hide();
-    $(window).scroll(function() {
-    	$('.navbar').fadeIn('slow');
-    	if ($(window).scrollTop() == 0) {
-    	$('.navbar').fadeOut('slow');
-    	};
-    });
+	$( "#navBoton" ).click(function() {
+	  openNav();
+	});
 
+	$( "#closeNavBtn" ).click(function() {
+	  closeNav();
+	});
+	
 });
+
+function openNav() {
+    document.getElementById("mySidenav").style.width = "250px";
+    document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
+}
+
+function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+    document.body.style.backgroundColor = "white";
+}
