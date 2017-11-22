cae57b199e5727b57e1a76d88d7b01cf707aae0c
David Artavia Rodriguez <dartavia@Coopelesca.co.cr>
2017-11-22 16:15:48 -0600

    Problema con top bar depurado

--- js/main.js
+++ js/main.js
@@ -1,27 +1,4 @@
 $( document ).ready(function() {
 
-	$("#openNav").click(function() {
-	  openNav();
-	});
-
-    $("#closeNav").click(function() {
-      closeNav();
-    });
-
-    $( '.sidenav a' ).on("click", function(){
-        closeNav();
-    });	
+	
 });
-
-
-function openNav() {
-    document.getElementById("mySidenav").style.width = "250px";
-    document.getElementById("main").style.marginLeft = "250px";
-    document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
-}
-
-function closeNav() {
-    document.getElementById("mySidenav").style.width = "0";
-    document.getElementById("main").style.marginLeft= "0";
-    document.body.style.backgroundColor = "white";
-}
